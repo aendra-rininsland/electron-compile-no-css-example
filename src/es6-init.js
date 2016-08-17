f92344d83a072b8e50038fe015ca4587e1325657
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-08-17 15:19:46 +0100

    Making build work.

--- src/es6-init.js
+++ src/es6-init.js
@@ -5,4 +5,4 @@ var appRoot = path.join(__dirname, '..');
 
 // ...and that your main app is called ./src/main.js. This is written as if
 // you were going to `require` the file from here.
-require('electron-compile').init(appRoot, './index');
+require('electron-compile').init(appRoot, path.resolve(__dirname, 'index.js'));
